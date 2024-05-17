a428c3f1e28af007a044fbfc971476d3fdc819e2
Muhammad Ahsan Kamal <ahsankamal9954@gmail.com>
2024-05-17 11:55:22 +0500

    button

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -54,6 +54,11 @@ export default async function Navbar() {
           </Suspense>
         </div>
       </div>
+      <div className="flex justify-end ml-4"> 
+            <button className="px-2 py-2 bg-black border border-white rounded-md items-center">
+              <img src="path/to/your/login_logo.svg" alt="Login Logo" className="w-6 h-6"></img>
+            </button>
+          </div>
     </nav>
   );
 }
--- components/logo-square.tsx
+++ components/logo-square.tsx
@@ -19,5 +19,6 @@ export default function LogoSquare({ size }: { size?: 'sm' | undefined }) {
         })}
       />
     </div>
+    
   );
 }
